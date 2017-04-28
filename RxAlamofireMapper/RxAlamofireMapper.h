a5834a584b8d178a5b243224ebce94854a089c23
Wassim  Seifeddine <wassim_seifeddine95@live.com>
2017-04-28 15:51:07 +0300

    Initial Commit

---
+++ RxAlamofireMapper/RxAlamofireMapper.h
@@ -0,0 +1,19 @@
+//
+//  RxAlamofireMapper.h
+//  RxAlamofireMapper
+//
+//  Created by Wassim on 4/28/17.
+//  Copyright © 2017 Wassim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxAlamofireMapper.
+FOUNDATION_EXPORT double RxAlamofireMapperVersionNumber;
+
+//! Project version string for RxAlamofireMapper.
+FOUNDATION_EXPORT const unsigned char RxAlamofireMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxAlamofireMapper/PublicHeader.h>
+
+
